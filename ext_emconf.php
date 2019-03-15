4553c26f9b320c3526027720f580cd45626b3390
Sascha Ende <sascha@sascha-ende.de>
2019-03-15 01:09:51 +0100

    Deleted    Classes/ViewHelpers/ObjectViewHelper.php Modified   Resources/Private/Partials/Debug.html Modified   Resources/Private/Templates/Boot1col.html Modified   ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,20 +2,20 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Gridelements Bootstrap 3',
-    'description' => 'Easily build grids with responsive bootstrap grids and extend gridelements by just adding partials and extending some typoscript.',
+    'description' => 'Easily build grids with responsive bootstrap grids and extend gridelements by just adding partials and extending some typoscript. Bootstrap 4 is planned.',
     'category' => 'plugin',
     'author' => 'Filmmusic.io',
-    'author_email' => 'sascha@sascha-ende.de',
+    'author_email' => 'info@filmmusic.io',
     'author_company' => 'Filmmusic.io',
-    'state' => 'alpha',
+    'state' => 'stable',
     'internal' => '',
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '0.0.4',
+    'version' => '1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-8.7.99',
+            'typo3' => '8.7.0-9.5.99',
             't3helpers' =>  '0.9.18',
             'gridelements'  => '8.2.3'
         ],
